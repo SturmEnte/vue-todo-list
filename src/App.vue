db4186646474753a0777673fc3f1503c1c6ef443
SturmEnte <jonas.nommensen@gmail.com>
2021-05-03 01:21:24 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Todo List</h1>
+  <button @click="createTodo" id="createTodoBtn">Create Todo</button>
+  <div id="todos">
+    <div v-for="todo in todos" :key="todo.id">
+      <Todo
+        :title="todo.title"
+        :finished="todo.finished"
+        :id="todo.id"
+        @deleteTodo="deleteTodo"
+        @toggleTodo="toggleFinished"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from "./components/Todo";
+
+import { v4 as uuid } from "uuid";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Todo },
+  data() {
+    return { todos: null };
+  },
+  mounted() {
+    this.todos = JSON.parse(localStorage.getItem("todos")) || [];
+  },
+  methods: {
+    createTodo() {
+      let todo = {
+        title: prompt("Todo Title"),
+        finished: false,
+        id: uuid(),
+      };
+      if (todo.title.length > 25) {
+        let titleArray = todo.title.split("");
+        let newTitle = [];
+        titleArray.forEach((titleSplit, index) => {
+          if (index + 1 < 25) {
+            newTitle.push(titleSplit);
+          }
+        });
+        todo.title = newTitle.join("");
+      }
+      this.todos.push(todo);
+      this.save();
+    },
+    deleteTodo(id) {
+      let todos = this.todos.filter((todo) => todo.id != id);
+      this.todos = todos;
+      this.save();
+    },
+    toggleFinished(id) {
+      let todos = this.todos;
+      let todo = todos.filter((todo) => todo.id === id)[0];
+      let i = todos.indexOf(todo);
+      todo.finished = !todo.finished;
+      todos[i] = todo;
+      this.todos = todos;
+      this.save();
+    },
+    save() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +77,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#todos {
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 10px;
+}
+#createTodoBtn {
+  border: 0;
+  color: white;
+  background: #2c3e50;
+  border-radius: 15px;
+  text-align: center;
+  padding: 5px;
+}
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="todo">
+    <h3>{{ title }}</h3>
+    <div id="util">
+      <input type="checkbox" :checked="finished" @click="toggleFinished" />
+      <button @click="deleteTodo">X</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: ["title", "finished", "id"],
+  methods: {
+    deleteTodo() {
+      this.$emit("deleteTodo", this.id);
+    },
+    toggleFinished() {
+      this.$emit("toggleFinished", this.id);
+    },
+  },
+};
+</script>
+
+<style>
+.todo {
+  background-color: #2c3e50;
+  color: white;
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+  flex-direction: row;
+  width: 400px;
+  height: 50px;
+  padding: 2px;
+  border-radius: 15px;
+  margin: 10px;
+  height: 3em;
+}
+.todo h3 {
+  margin: auto 0;
+  margin-left: 10px;
+}
+.todo #util {
+  margin: auto 0;
+  margin-right: 10px;
+}
+.todo button {
+  color: #2c3e50;
+  background: white;
+  border-radius: 100px;
+  width: 2em;
+  height: 2em;
+  text-align: center;
+  border: none;
+}
+.todo input {
+  width: 1em;
+  height: 1em;
+}
+</style>
